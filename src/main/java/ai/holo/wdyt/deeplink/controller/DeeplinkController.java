41af6bf43bf44a66ee20e7e22b940fa25a2e6419
alissaglam <alisaglam@gmail.com>
2025-02-24 16:41:04 +0000

    adjust deep linking urls

--- src/main/java/ai/holo/wdyt/deeplink/controller/DeeplinkController.java
+++ src/main/java/ai/holo/wdyt/deeplink/controller/DeeplinkController.java
@@ -3,21 +3,14 @@ package ai.holo.wdyt.deeplink.controller;
 import ai.holo.wdyt.deeplink.model.dto.ReferralLinkDto;
 import ai.holo.wdyt.deeplink.model.dto.SaveUserFingerprintDto;
 import ai.holo.wdyt.deeplink.service.DeeplinkService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Blob;
 
 @RestController
 @RequestMapping("/api/v1/referral")
@@ -43,7 +36,7 @@ public class DeeplinkController {
         deeplinkService.useReferralWithFingerprint(fingerprint);
     }
 
-    @GetMapping("/redirect/referral/{nonce}")
+    @GetMapping("/use/{nonce}")
     public ResponseEntity<Resource> getDeepLinkForReferral(@PathVariable String nonce) throws IOException {
         String content = deeplinkService.getDeeplinkReferralRedirectHtml(nonce);
         return ResponseEntity.ok()
