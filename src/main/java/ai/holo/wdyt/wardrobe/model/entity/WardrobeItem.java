1b78e013d98b0eda84d6c5d1d589123302140322
omerfaruksaglam <omrfrksglm06@gmail.com>
2025-07-15 20:31:38 +0300

    Add wardrobe and wardrobe item entities

---
+++ src/main/java/ai/holo/wdyt/wardrobe/model/entity/WardrobeItem.java
@@ -0,0 +1,33 @@
+package ai.holo.wdyt.wardrobe.model.entity;
+
+import ai.holo.wdyt.askai.model.entity.TagConverter;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Entity(name = "wardrobe_item")
+@Getter
+@Setter
+@NoArgsConstructor
+public class WardrobeItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "image_path")
+    private String imagePath;
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private Category type;
+    @Column(name = "liked")
+    private boolean liked;
+    @Convert(converter = TagConverter.class)
+    @Column(columnDefinition = "JSON")
+    private Map<String, List<String>> tags = new HashMap<>();
+}
